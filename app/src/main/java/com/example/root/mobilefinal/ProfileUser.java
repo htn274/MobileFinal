9d58fe1ec65e0b80af36b69184465fb38eba2a4a
htn274 <thiennust274@gmail.com>
2019-01-11 00:41:11 +0700

    update home and profile ui

--- app/src/main/java/com/example/root/mobilefinal/ProfileUser.java
+++ app/src/main/java/com/example/root/mobilefinal/ProfileUser.java
@@ -1,8 +1,13 @@
 package com.example.root.mobilefinal;
 
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +19,38 @@ import android.view.ViewGroup;
 public class ProfileUser extends Fragment {
 
 
+    private FragmentActivity myContext;
+    private ViewPager pager;
+    private TabLayout tabLayout;
+
     public ProfileUser() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Activity activity) {
+        myContext=(FragmentActivity) activity;
+        super.onAttach(activity);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_user, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile_user, container, false);
+        addControl(view);
+        return view;
     }
 
+    private void addControl(View view) {
+        pager = (ViewPager) view.findViewById(R.id.view_pager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
+        FragmentManager manager = myContext.getSupportFragmentManager();
+        PageAdapter adapter = new PageAdapter(manager);
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setTabsFromPagerAdapter(adapter);//deprecated
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(pager));
+    }
 }

f495a1e9ea76cda4de79bc19d3156b865dd0cd61
htn274 <thiennust274@gmail.com>
2019-01-11 23:16:23 +0700

    pick image

--- app/src/main/java/com/example/root/mobilefinal/AddShopForm.java
+++ app/src/main/java/com/example/root/mobilefinal/AddShopForm.java
@@ -1,12 +1,16 @@
 package com.example.root.mobilefinal;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-public class AddShopForm extends AppCompatActivity {
+public class AddShopForm extends AppCompatActivity implements View.OnClickListener {
 
     ImageView avatar;
     Button btn_uploadAvatar, btn_next;
@@ -22,18 +26,50 @@ public class AddShopForm extends AppCompatActivity {
     void initView(){
         avatar = findViewById(R.id.imageView_avatarShop);
         btn_uploadAvatar = findViewById(R.id.btn_uploadAvatar);
+        btn_uploadAvatar.setOnClickListener((View.OnClickListener) this);
         btn_next = findViewById(R.id.btn_nextStep);
         editText_shopName = findViewById(R.id.editText_shopname);
         editText_location = findViewById(R.id.editText_location);
 
         editText_openHour = findViewById(R.id.editText_openHour);
+        editText_openHour.setInputType(InputType.TYPE_NULL);
         editText_closeHour = findViewById(R.id.EditText_closeHour);
+        editText_closeHour.setInputType(InputType.TYPE_NULL);
         setTime();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btn_uploadAvatar){
+            onPickImage(view);
+        }
     }
 
     void setTime(){
         TimePickerFragment pickOpenHour = new TimePickerFragment(editText_openHour, this);
         TimePickerFragment pickCloseHour = new TimePickerFragment(editText_closeHour, this);
+    }
+
+//    Choose image
+    private static final int PICK_IMAGE_ID = 234; // the number doesn't matter
+
+    public void onPickImage(View view) {
+        Intent chooseImageIntent = ImagePicker.getPickImageIntent(this);
+        startActivityForResult(chooseImageIntent, PICK_IMAGE_ID);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case PICK_IMAGE_ID:
+                Bitmap bitmap = ImagePicker.getImageFromResult(this, resultCode, data);
+                // TODO use bitmap
+                avatar.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 200,200, false));
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/example/root/mobilefinal/MyShopFragment.java
+++ app/src/main/java/com/example/root/mobilefinal/MyShopFragment.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
@@ -30,10 +31,14 @@ public class MyShopFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_my_shop, container, false);
-
         btn_add = view.findViewById(R.id.btn_addShop);
-        btn_add.setOnClickListener((View.OnClickListener) myContext.getApplicationContext());
+        btn_add.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override

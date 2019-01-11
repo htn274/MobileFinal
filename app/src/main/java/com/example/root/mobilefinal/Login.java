41aaad6ccbf2cc30b6b9eeb9793c9cccd57ccb39
bao <baosws@gmail.com>
2019-01-11 18:38:45 +0700

    ecec

--- app/src/main/java/com/example/root/mobilefinal/Login.java
+++ app/src/main/java/com/example/root/mobilefinal/Login.java
@@ -1,13 +1,75 @@
 package com.example.root.mobilefinal;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
+import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    Button btnSignIn;
+    EditText editTextPhoneNumber;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FirebaseApp.initializeApp(this);
+        editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        auth = FirebaseAuth.getInstance();
+
+        btnSignIn = findViewById(R.id.btn_login);
+        btnSignIn.setOnClickListener(this);
+    }
+
+    void signInWithEmailPassword(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_LONG).show();
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("btag", "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // ...
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w("btag", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Login faled", Toast.LENGTH_LONG).show();
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_login) {
+            signInWithEmailPassword("baosws@gmail.com", "123456");
+        }
     }
 }

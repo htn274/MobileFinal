1d1361a1307e6e8997d3093b05ecb6ca9123f971
htn274 <thiennust274@gmail.com>
2019-01-11 09:57:00 +0700

    add shop and item

--- app/src/main/java/com/example/root/mobilefinal/AddShopForm.java
+++ app/src/main/java/com/example/root/mobilefinal/AddShopForm.java
@@ -3,12 +3,11 @@ package com.example.root.mobilefinal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class AddShopForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_add_shop_form);
     }
 }

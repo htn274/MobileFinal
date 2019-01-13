40e7b43e0ba186cf969fb6b75e7dd1b9d9885825
htn274 <thiennust274@gmail.com>
2019-01-13 23:27:15 +0700

    update shop detail

---
+++ app/src/main/java/com/example/root/mobilefinal/ShopDetailPageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.root.mobilefinal;
+
+public class ShopDetailPageAdapter {
+}
---
+++ app/src/main/java/com/example/root/mobilefinal/ShopListItemsFragment.java
@@ -0,0 +1,29 @@
+package com.example.root.mobilefinal;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShopListItems extends Fragment {
+
+
+    public ShopListItems() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shop_list_items, container, false);
+    }
+
+}

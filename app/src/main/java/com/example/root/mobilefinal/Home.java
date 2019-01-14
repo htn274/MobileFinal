9f86af74d3bbe7c9817f6a38270406b38c506db9
htn274 <thiennust274@gmail.com>
2019-01-15 05:01:31 +0700

    fix toolbar

--- app/src/main/java/com/example/root/mobilefinal/Home.java
+++ app/src/main/java/com/example/root/mobilefinal/Home.java
@@ -3,6 +3,7 @@ package com.example.root.mobilefinal;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,5 +26,4 @@ public class Home extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
 }
